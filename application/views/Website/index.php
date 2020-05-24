1a867def22d4b660d5e65209b4c42fec4c45fa6c
technothinksup <projects.technothinksup@gmail.com>
2020-05-24 14:34:31 +0530

    first commit

---
+++ application/views/Website/index.php
@@ -0,0 +1,290 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+    <title>Guruprasad Auto </title>
+  </head>
+  <body>
+
+    <section class="top-head">
+      <div class="row">
+        <div class="col-md-6">
+        <span class="mr-4">  <i class="fas fa-phone-alt mr-2"></i>Call : 9876543210 </span>   <span><i class="far fa-envelope mr-2"></i>Mail : info@guruprasad.com</span>
+        </div>
+        <div class="col-md-4 offset-md-2 text-center pr-5">
+          <i class="fab fa-google mr-2 f-16"></i> <i class="fab fa-facebook-f mr-2 f-16"></i><i class="fab fa-linkedin-in mr-2 f-16"></i><i class="fab fa-youtube f-16 mr-2"></i>
+        </div>
+      </div>
+    </section>
+
+    <section class="logo-sec">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="row">
+              <div class="col-3">
+                <i class="fas fa-globe f-50  c-orange"></i>
+              </div>
+              <div class="col-9 bl-1">
+                <span class="f-18  ">Plot No. W-2 MIDC Shiroli, Kolhapur 416122</span>
+              </div>
+            </div>
+
+
+          </div>
+          <div class="col-md-4 text-center">
+            <img class="main-logo" src="<?php echo base_url(); ?>assets/images/guru_logo.png" alt="">
+          </div>
+          <div class="col-md-4">
+            <div class="row">
+              <div class="col-9 br-1 text-right">
+                <span class="f-18 ml-2">Opning Hours </span>
+                <p class="f-16 ml-2 mb-0">10.00AM - 7.00PM </p>
+              </div>
+              <div class="col-3">
+                  <i class="fas fa-globe f-50 float-right mr-2 c-orange"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+  <section class="main-navbar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item active">
+        <a class="nav-link no-border" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Our Products</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Workshop</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Awards</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  </section>
+
+
+  <section class="slider" >
+   <div class="container-fluid">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img  class="slider-img" class="d-block w-100" src="<?php echo base_url(); ?>assets/images/slide3.png" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="slider-img" class="d-block w-100" src="<?php echo base_url(); ?>assets/images/slide2.png" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="slider-img" class="d-block w-100" src="<?php echo base_url(); ?>assets/images/slide1.png" alt="Third slide">
+            </div>
+
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+          </div>
+      </div>
+    </section>
+
+    <section class="about-home">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="big-heading text-center font-bold">Welcome To Kolhapur Engineering Association</h1>
+            <p>Kolhapur Engineering Association is the catalyst required by every industry to start any reaction. The efforts of Kolhapur Engineering Association, let them be in interacting with Government officials or portraying the problems of industrialists as a whole or encouraging industrial growth and quality, all help to start a chain reaction resulting in huge rewards and benefits to every industry. Also Kolhapur Engineering Association today has proven itself to be a representative of a group of industries that are strong and capable performers beyond global standards.</p>
+            <p>Kolhapur Engineering Association also plays an active role in creating an environment conductive to growth and provides opportunities to its industries to reach international markets. From agriculture to Information technology Kolhapur Engineering Association has members from all sectors and types of industry.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="counter-sec ">
+      <div class="container-fluid">
+        <div class="container">
+          <div class="counter-div">
+              <div class="row">
+                <div class="col-md-12">
+                  <h2 class="text-center ">Numerical Facts</h2>
+                </div>
+                <div class="col-md-3 text-center">
+                    <i class="fas fa-globe f-50 c-orange "></i>
+                    <p class="count" >10 <sup>+</sup>  </p>
+                    <p class="count-text">Years of Legacy</p>
+                </div>
+                <div class="col-md-3 text-center">
+                    <i class="fas fa-globe f-50 c-orange "></i>
+                    <p class="count" >10 <sup>+</sup>  </p>
+                    <p class="count-text">Years of Legacy</p>
+                </div>
+                <div class="col-md-3 text-center">
+                    <i class="fas fa-globe f-50 c-orange "></i>
+                    <p class="count" >10 <sup>+</sup>  </p>
+                    <p class="count-text">Years of Legacy</p>
+                </div>
+                <div class="col-md-3 text-center">
+                    <i class="fas fa-globe f-50 c-orange "></i>
+                    <p class="count" >10 <sup>+</sup>  </p>
+                    <p class="count-text">Years of Legacy</p>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+
+    </section>
+
+    <section class="content-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="sub-heading ">Objectives Of Association</h2>
+          </div>
+          <div class="col-md-6">
+            <ul>
+              <li>To promote healthy growth and development of industries.</li>
+              <li>To study the legislative, executive and other actions of the Union and State Governments.</li>
+              <li>To create public awareness/ opinion with regard to the character scope, importance and need of industrial enterprise in the country.</li>
+              <li>To try to settle by arbitration, commercial and industrial disputes with all concerned government bodies.</li>
+              <li>To create healthy, interaction with industries and create other amenities which are useful and beneficial to the industrial community.</li>
+              <li>To create healthy, interaction with industries and create other amenities which are useful and beneficial to the industrial community.</li>
+            </ul>
+          </div>
+
+          <div class="col-md-6">
+            <img src="<?php echo base_url(); ?>assets/images/right.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+
+  <section class="product-section">
+    <div class="container ">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="sub-heading"> Product Section</h2>
+       </div>
+       <div class="col-md-4">
+         <div class="card">
+            <img class="card-img-top" src="<?php echo base_url(); ?>assets/images/product01.jpg" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">Turbine Housing Mid Range (MR) Waste Gate With Cup Plus</p>
+            </div>
+          </div>
+       </div>
+
+       <div class="col-md-4">
+         <div class="card">
+            <img class="card-img-top" src="<?php echo base_url(); ?>assets/images/product01.jpg" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">Turbine Housing Mid Range (MR) Waste Gate With Cup Plus</p>
+            </div>
+          </div>
+       </div>
+
+       <div class="col-md-4">
+         <div class="card">
+            <img class="card-img-top" src="<?php echo base_url(); ?>assets/images/product01.jpg" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">Turbine Housing Mid Range (MR) Waste Gate With Cup Plus</p>
+            </div>
+          </div>
+       </div>
+
+       <div class="col-md-12">
+         <div class="w-100 text-center btn-product-link">
+          <a href="#"> <button type="button" class="btn btn-outline-danger">View More</button> </a>
+         </div>
+       </div>
+
+     </div>
+    </div>
+  </section>
+
+  <section class="product-section">
+    <div class="container ">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="sub-heading"> Product Section</h2>
+       </div>
+       <div class="col-md-4">
+         <div class="card">
+            <img class="card-img-top" src="<?php echo base_url(); ?>assets/images/product01.jpg" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">Turbine Housing Mid Range (MR) Waste Gate With Cup Plus</p>
+            </div>
+          </div>
+       </div>
+
+       <div class="col-md-4">
+         <div class="card">
+            <img class="card-img-top" src="<?php echo base_url(); ?>assets/images/product01.jpg" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">Turbine Housing Mid Range (MR) Waste Gate With Cup Plus</p>
+            </div>
+          </div>
+       </div>
+
+       <div class="col-md-4">
+         <div class="card">
+            <img class="card-img-top" src="<?php echo base_url(); ?>assets/images/product01.jpg" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">Turbine Housing Mid Range (MR) Waste Gate With Cup Plus</p>
+            </div>
+          </div>
+       </div>
+
+       <div class="col-md-12">
+         <div class="w-100 text-center btn-product-link">
+          <a href="#"> <button type="button" class="btn btn-outline-danger">View More</button> </a>
+         </div>
+       </div>
+
+     </div>
+    </div>
+  </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
